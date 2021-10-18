4a3f7a4179e34e5a6904ebfae288b1473e61bca8
Alessandro Zanzi <alessandro.zanzi2@studio.unibo.it>
2021-10-18 17:03:40 +0200

    Return to menu option

--- src/view/OptionGUI.java
+++ src/view/OptionGUI.java
@@ -1,5 +1,8 @@
 package view;
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import javax.swing.JFrame;
 
 public class OptionGUI extends JFrame {
@@ -11,6 +14,14 @@ public class OptionGUI extends JFrame {
 
 	public OptionGUI(MenuGUI menuGUI) {
 		// TODO Auto-generated constructor stub
+		
+		this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		this.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(final WindowEvent e) {
+				MyOptionPane.returnToMenu(OptionGUI.this, menuGUI);
+			}
+		});
 	}
 
 }
