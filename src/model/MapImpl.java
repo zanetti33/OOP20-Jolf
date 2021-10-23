e313f67781fd44f7e8d50b88ce9790e392754058
Alessandro Zanzi <alessandro.zanzi2@studio.unibo.it>
2021-10-23 13:47:42 +0200

    New maps

--- src/model/MapImpl.java
+++ src/model/MapImpl.java
@@ -52,24 +52,25 @@ public class MapImpl implements Map {
 				return new MapImpl(new Dimension(720, 400),
 						new Ball(new Point(30, 200)),
 						List.of(),
-						List.of(new Wall(new Point2D(130, 130), 300, 30), new Wall(new Point2D(130, 240), 300, 30),
-								new Sand(new Point2D(130, 0), 300, 80), new Sand(new Point2D(130, 160), 300, 80), new Sand(new Point2D(500, 40), 300, 80)),
-						List.of(new Star(new Point2D(45, 100)), new Star(new Point2D(220, 350)), new Star(new Point2D(600, 300))));
+						List.of(new Wall(new Point2D(110, 120), 300, 30), new Wall(new Point2D(180, 250), 300, 30), new Wall(new Point2D(570, 120), 30, 200),
+								new Sand(new Point2D(180, 150), 230, 100), new Sand(new Point2D(600, 120), 150, 200)),
+						List.of(new Star(new Point2D(300, 45)), new Star(new Point2D(220, 350)), new Star(new Point2D(600, 300))));
 			case HOLE_4:
 				return new MapImpl(new Dimension(720, 400),
-						new Ball(new Point(80, 200)),
+						new Ball(new Point(600, 350)),
 						List.of(),
-						List.of(new Wall(new Point2D(130, 130), 300, 30), new Wall(new Point2D(130, 240), 300, 30),
+						List.of(new Wall(new Point2D(370, 0), 30, 180), new Wall(new Point2D(130, 240), 300, 30), new Wall(new Point2D(150, 0), 30, 100), new Wall(new Point2D(0, 150), 100, 30),
 								new Ice(new Point2D(130, 270), 300, 130),
-								new Sand(new Point2D(130, 160), 300, 80), new Sand(new Point2D(220, 400), 300, 80)),
-						List.of(new Star(new Point2D(320, 100)), new Star(new Point2D(60, 350)), new Star(new Point2D(220, 30))));
+								new Sand(new Point2D(400, 0), 120, 180), new Sand(new Point2D(220, 400), 300, 80)),
+						List.of(new Star(new Point2D(600, 50)), new Star(new Point2D(250, 50)), new Star(new Point2D(150, 300))));
 			case HOLE_5:
 				return new MapImpl(new Dimension(720, 400),
 						new Ball(new Point(10, 200)),
 						List.of(),
-						List.of(new Wall(new Point2D(130, 130), 300, 30), new Wall(new Point2D(130, 240), 300, 30),
+						List.of(new Wall(new Point2D(130, 130), 300, 30), new Wall(new Point2D(130, 240), 300, 30), new Wall(new Point2D(550, 150), 30, 100),
+								 new Wall(new Point2D(580, 150), 50, 20), new Wall(new Point2D(670, 150), 50, 20), new Wall(new Point2D(580, 230), 50, 20), new Wall(new Point2D(670, 230), 50, 20),
 								new Ice(new Point2D(130, 0), 300, 130), new Ice(new Point2D(130, 270), 300, 130), new Ice(new Point2D(130, 350), 300, 130)),
-						List.of(new Star(new Point2D(380, 60)), new Star(new Point2D(200, 100)), new Star(new Point2D(125, 350))));
+						List.of(new Star(new Point2D(380, 60)), new Star(new Point2D(600, 190)), new Star(new Point2D(125, 350))));
 			default:
 				return null;
 		}
